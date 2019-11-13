7605f591484a3cd13a0a79df96061da16e915f07
Mauricio Villegas <mauricio_ville@yahoo.com>
2019-11-13 11:34:26 +0100

    Fixed tests in circleci config.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.10.0',
+    VERSION: 'master/v2.10.0+1commits/2019.11.13',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
