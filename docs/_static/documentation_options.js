08915c07f8811a7302f1e6488791991bbc82639a
Mauricio Villegas <mauricio_ville@yahoo.com>
2019-11-30 10:56:17 +0100

    - Fixing code climate coverage.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master/v2.11.0+1commits/2019.11.30',
+    VERSION: 'master/v2.11.0+2commits/2019.11.30',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
