e0a4d75e457a47d44a5fb683e742f69f462489e3
Mauricio Villegas <mauricio_ville@yahoo.com>
2019-11-30 11:10:52 +0100

    Minor change to circleci config.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.12.0',
+    VERSION: 'master/v2.12.0+1commits/2019.11.30',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
