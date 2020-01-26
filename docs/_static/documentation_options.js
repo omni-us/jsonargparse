c8453fe013f8f3e68b45e5843a2564ea394afab2
Mauricio Villegas <mauricio_ville@yahoo.com>
2020-01-26 21:11:40 +0100

    Bump version: 2.22.1 → 2.22.2

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master/v2.22.1+1commits/2020.01.26',
+    VERSION: '2.22.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- jsonargparse.py
+++ jsonargparse.py
@@ -35,7 +35,7 @@ except Exception as ex:
     _jsonnet = ex
 
 
-__version__ = '2.22.1'
+__version__ = '2.22.2'
 
 
 meta_keys = {'__cwd__', '__path__'}
