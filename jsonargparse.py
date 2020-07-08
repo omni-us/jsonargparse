c2eaba138f6e9d680a52f1a41f2b7d6c84579dfd
Mauricio Villegas <mauricio_ville@yahoo.com>
2020-07-08 16:28:01 +0200

    Bump version: 2.31.1 → 2.31.2

--- jsonargparse.py
+++ jsonargparse.py
@@ -64,7 +64,7 @@ def import_requests(importer):
         raise ImportError('requests package is required by '+importer+' :: '+str(ex))
 
 
-__version__ = '2.31.1'
+__version__ = '2.31.2'
 
 
 meta_keys = {'__cwd__', '__path__'}
