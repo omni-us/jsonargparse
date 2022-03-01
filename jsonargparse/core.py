omni-us/jsonargparse

commit 97e45675926fb192ebad1c513e0334310e160899

ArgumentParser objects are now pickleable pytorch-lightning#12011.

jsonargparse/core.py
@@ -7,12 +7,11 @@ import logging
 import os
 import re
 import sys
-from contextlib import redirect_stderr
 from copy import deepcopy
 from typing import Any, Callable, Dict, List, NoReturn, Optional, Sequence, Set, Tuple, Type, Union
 from unittest.mock import patch
 
-from .formatters import DefaultHelpFormatter, empty_help
+from .formatters import DefaultHelpFormatter, empty_help, formatter_context, get_env_var
 from .jsonnet import ActionJsonnet
 from .jsonschema import ActionJsonSchema
 from .loaders_dumpers import check_valid_dump_format, dump_using_format, get_loader_exceptions, loaders, load_value, load_value_context
@@ -44,12 +43,12 @@ from .optionals import (
     import_fsspec,
 )
 from .util import (
+    identity,
     ParserError,
     usage_and_exit_error_handler,
     change_to_path_dir,
     Path,
     LoggerProperty,
-    _get_env_var,
     _suppress_stderr,
     _lenient_check_context,
     lenient_check,
@@ -67,6 +66,7 @@ class _ActionsContainer(SignatureArguments, argparse._ActionsContainer, LoggerPr
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.register('type', None, identity)
         self.register('action', 'parsers', _ActionSubCommands)
 
 
@@ -182,20 +182,15 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
             default_env: Set the default value on whether to parse environment variables.
             default_meta: Set the default value on whether to include metadata in config objects.
         """
-        class FormatterClass(formatter_class):  # type: ignore
-            _parser = self
-
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, formatter_class=formatter_class, **kwargs)
         if self.groups is None:
             self.groups = {}
         self.required_args: Set[str] = set()
         self.save_path_content: Set[str] = set()
-        self._stderr = sys.stderr
         self.default_config_files = default_config_files
         self.default_meta = default_meta
         self.default_env = default_env
         self.env_prefix = env_prefix
-        self.formatter_class = FormatterClass
         self.parser_mode = parser_mode
         self.logger = logger
         self.error_handler = error_handler
@@ -410,11 +405,11 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
         cfg = Namespace()
         actions = filter_default_actions(self._actions)
         for action in actions:
-            env_var = _get_env_var(self, action)
+            env_var = get_env_var(self, action)
             if env_var in env and isinstance(action, ActionConfigFile):
                 ActionConfigFile.apply_config(self, cfg, action.dest, env[env_var])
         for action in actions:
-            env_var = _get_env_var(self, action)
+            env_var = get_env_var(self, action)
             if env_var in env and isinstance(action, _ActionSubCommands):
                 env_val = env[env_var]
                 if env_val in action.choices:
@@ -423,7 +418,7 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
                     for k, v in vars(pcfg).items():
                         cfg[subcommand+'.'+k] = v
         for action in actions:
-            env_var = _get_env_var(self, action)
+            env_var = get_env_var(self, action)
             if env_var in env and not isinstance(action, ActionConfigFile):
                 env_val = env[env_var]
                 if _is_action_value_list(action):
@@ -715,7 +710,8 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
         with load_value_context(self.parser_mode):
             cleanup_actions(cfg, self._actions)
 
-        return dump_using_format(self, cfg.as_dict(), 'yaml_comments' if yaml_comments else format)
+        with formatter_context(self):
+            return dump_using_format(self, cfg.as_dict(), 'yaml_comments' if yaml_comments else format)
 
 
     def save(
@@ -806,7 +802,7 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
                             f.write(val.get_content())
                         cfg[key] = type(val)(str(val_path))
 
-            with change_to_path_dir(path_fc):
+            with change_to_path_dir(path_fc), formatter_context(self):
                 save_paths(cfg)
             dump_kwargs['skip_check'] = True
             with open(path_fc(), 'w') as f:
@@ -933,7 +929,7 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
         """Logs error message if a logger is set, calls the error handler and raises a ParserError."""
         self._logger.error(message)
         if self._error_handler is not None:
-            with redirect_stderr(self._stderr):
+            with _suppress_stderr():
                 self._error_handler(self, message)
         if ex is None:
             raise ParserError(message)
@@ -1106,6 +1102,7 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
 
 
     def format_help(self) -> str:
+        defaults = None
         if len(self._default_config_files) > 0:
             note = 'no existing default config file found.'
             try:
@@ -1115,13 +1112,12 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
                     if isinstance(config_files, list):
                         config_files = [str(x) for x in config_files]
                     note = f'default values below are the ones overridden by the contents of: {config_files}'
-                    self.formatter_class.defaults = defaults
             except ParserError as ex:
                 note = f'tried getting defaults considering default_config_files but failed due to: {ex}'
             group = self._default_config_files_group
             group.description = f'{self._default_config_files}, Note: {note}'
-        help_str = super().format_help()
-        self.formatter_class.defaults = None
+        with formatter_context(self, defaults):
+            help_str = super().format_help()
         return help_str
 
 

jsonargparse_tests/core_tests.py
@@ -3,7 +3,9 @@
 import calendar
 import json
 import os
+import pickle
 import platform
+import sys
 import unittest
 import warnings
 import yaml
@@ -1075,6 +1077,13 @@ class OtherTests(unittest.TestCase):
     def test_parse_args_invalid_args(self):
         parser = ArgumentParser(error_handler=None)
         self.assertRaises(ParserError, lambda: parser.parse_args([{}]))
+
+
+    @unittest.skipIf(sys.version_info[:2] == (3, 6), 'loggers not pickleable in python 3.6')
+    def test_pickle_parser(self):
+        parser1 = example_parser()
+        parser2 = pickle.loads(pickle.dumps(parser1))
+        self.assertEqual(parser1.get_defaults(), parser2.get_defaults())
 
 
 if __name__ == '__main__':

jsonargparse/formatters.py
@@ -1,11 +1,13 @@
 """Formatter classes."""
 
 import re
-from argparse import _HelpAction, HelpFormatter, OPTIONAL, SUPPRESS, ZERO_OR_MORE
+from argparse import Action, _HelpAction, HelpFormatter, OPTIONAL, SUPPRESS, ZERO_OR_MORE
+from contextlib import contextmanager
+from contextvars import ContextVar
 from enum import Enum
 from io import StringIO
 from string import Template
-from typing import Optional
+from typing import Optional, Union
 
 from .actions import (
     ActionConfigFile,
@@ -20,13 +22,27 @@ from .namespace import Namespace
 from .optionals import import_ruyaml
 from .type_checking import ArgumentParser, ruyamlCommentedMap
 from .typehints import ActionTypeHint, type_to_str
-from .util import _get_env_var
 
 
 __all__ = ['DefaultHelpFormatter']
 
 
 empty_help: str = '_EMPTY_HELP_'
+
+
+formatter_parser: ContextVar = ContextVar('formatter_parser')
+formatter_defaults: ContextVar = ContextVar('formatter_defaults')
+
+
+@contextmanager
+def formatter_context(parser: 'ArgumentParser', defaults: Optional[Namespace] = None):
+    prev_parser = formatter_parser.set(parser)
+    prev_defaults = formatter_defaults.set(defaults)
+    try:
+        yield
+    finally:
+        formatter_parser.reset(prev_parser)
+        formatter_defaults.reset(prev_defaults)
 
 
 class PercentTemplate(Template):
@@ -50,10 +66,6 @@ class DefaultHelpFormatter(HelpFormatter):
     with :code:`default_env=True` command line options are preceded by 'ARG:' and
     the respective environment variable name is included preceded by 'ENV:'.
     """
-
-    _parser: 'ArgumentParser'
-    defaults: Optional[Namespace] = None
-
 
     def _get_help_string(self, action):
         action_help = ' ' if action.help == empty_help else action.help
@@ -79,11 +91,12 @@ class DefaultHelpFormatter(HelpFormatter):
 
 
     def _format_action_invocation(self, action):
-        if action.option_strings == [] or action.default == SUPPRESS or not self._parser.default_env:
+        parser = formatter_parser.get()
+        if action.option_strings == [] or action.default == SUPPRESS or not parser.default_env:
             return super()._format_action_invocation(action)
         extr = ''
-        if self._parser.default_env:
-            extr += '\n  ENV:   ' + _get_env_var(self, action)
+        if parser.default_env:
+            extr += '\n  ENV:   ' + get_env_var(self, action)
         return 'ARG:   ' + super()._format_action_invocation(action) + extr
 
 
@@ -106,8 +119,9 @@ class DefaultHelpFormatter(HelpFormatter):
         if type_str is not None:
             params['type'] = type_str
         if 'default' in params:
-            if self.defaults is not None:
-                params['default'] = self.defaults.get(action.dest)
+            defaults = formatter_defaults.get()
+            if defaults is not None:
+                params['default'] = defaults.get(action.dest)
             if params['default'] is None:
                 params['default'] = 'null'
             elif isinstance(params['default'], Enum) and hasattr(params['default'], 'name'):
@@ -148,8 +162,9 @@ class DefaultHelpFormatter(HelpFormatter):
                     subparsers.update(get_subparsers(subparser, prefix=full_key))
             return subparsers
 
-        parsers = get_subparsers(self._parser)
-        parsers[None] = self._parser
+        parser = formatter_parser.get()
+        parsers = get_subparsers(parser)
+        parsers[None] = parser
 
         group_titles = {}
         for parser_key, parser in parsers.items():
@@ -165,7 +180,7 @@ class DefaultHelpFormatter(HelpFormatter):
         def set_comments(cfg, prefix='', depth=0):
             for key in cfg.keys():
                 full_key = (prefix+'.' if prefix else '')+key
-                action = _find_action(self._parser, full_key)
+                action = _find_action(parser, full_key)
                 text = None
                 if full_key in group_titles and isinstance(cfg[key], dict):
                     text = group_titles[full_key]
@@ -178,8 +193,8 @@ class DefaultHelpFormatter(HelpFormatter):
                 elif text:
                     self.set_yaml_argument_comment(text, cfg, key, depth)
 
-        if self._parser.description is not None:
-            self.set_yaml_start_comment(self._parser.description, cfg)
+        if parser.description is not None:
+            self.set_yaml_start_comment(parser.description, cfg)
         set_comments(cfg)
         out = StringIO()
         yaml.dump(cfg, out)
@@ -234,3 +249,12 @@ class DefaultHelpFormatter(HelpFormatter):
             depth: The nested level of the argument.
         """
         cfg.yaml_set_comment_before_after_key(key, before='\n'+text, indent=2*depth)
+
+
+def get_env_var(parser_or_formatter: Union['ArgumentParser', DefaultHelpFormatter], action: Action) -> str:
+    """Returns the environment variable for a given parser or formatter and action."""
+    if isinstance(parser_or_formatter, DefaultHelpFormatter):
+        parser_or_formatter = formatter_parser.get()
+    env_var = (parser_or_formatter._env_prefix+'_' if parser_or_formatter._env_prefix else '') + action.dest
+    env_var = env_var.replace('.', '__').upper()
+    return env_var

jsonargparse/util.py
@@ -7,7 +7,6 @@ import re
 import stat
 import sys
 import warnings
-from argparse import Action
 from collections import defaultdict
 from contextlib import contextmanager, redirect_stderr
 from contextvars import ContextVar
@@ -34,8 +33,9 @@ __all__ = [
 ]
 
 
-null_logger = logging.Logger('jsonargparse_null_logger')
+null_logger = logging.getLogger('jsonargparse_null_logger')
 null_logger.addHandler(logging.NullHandler())
+null_logger.parent = None
 
 
 NoneType = type(None)
@@ -56,6 +56,10 @@ def warning(message, category=JsonargparseWarning, stacklevel=1):
         category=category,
         stacklevel=stacklevel+1,
     )
+
+
+def identity(value):
+    return value
 
 
 def _parse_value_or_config(value: Any, enable_path: bool = True) -> Tuple[Any, Optional['Path']]:
@@ -88,15 +92,6 @@ def usage_and_exit_error_handler(parser: 'ArgumentParser', message: str) -> None
     args = {'prog': parser.prog, 'message': message}
     sys.stderr.write('%(prog)s: error: %(message)s\n' % args)
     parser.exit(2)
-
-
-def _get_env_var(parser: 'ArgumentParser', action: 'Action') -> str:
-    """Returns the environment variable for a given parser and action."""
-    if hasattr(parser, '_parser'):
-        parser = parser._parser
-    env_var = (parser._env_prefix+'_' if parser._env_prefix else '') + action.dest
-    env_var = env_var.replace('.', '__').upper()
-    return env_var
 
 
 def _issubclass(cls, class_or_tuple):

commit bfeebb78f7cbd4434cb5231f12c7c36c4c5d5766

Regression introduced in commit 97e4567 fixed and updated unit test to prevent it #128.

jsonargparse/core.py
@@ -936,8 +936,7 @@ class ArgumentParser(_ActionsContainer, argparse.ArgumentParser):
         """Logs error message if a logger is set, calls the error handler and raises a ParserError."""
         self._logger.error(message)
         if self._error_handler is not None:
-            with _suppress_stderr():
-                self._error_handler(self, message)
+            self._error_handler(self, message)
         if ex is None:
             raise ParserError(message)
         else:

jsonargparse_tests/core_tests.py
@@ -10,7 +10,7 @@ import unittest
 import warnings
 import yaml
 from io import StringIO
-from contextlib import redirect_stdout
+from contextlib import redirect_stderr, redirect_stdout
 from collections import OrderedDict
 from random import randint, shuffle
 from jsonargparse import (
@@ -1036,11 +1036,13 @@ class OtherTests(unittest.TestCase):
 
 
     def test_usage_and_exit_error_handler(self):
-        with _suppress_stderr():
+        err = StringIO()
+        with redirect_stderr(err):
             parser = ArgumentParser()
             parser.add_argument('--val', type=int)
             self.assertEqual(8, parser.parse_args(['--val', '8']).val)
             self.assertRaises(SystemExit, lambda: parser.parse_args(['--val', 'eight']))
+        self.assertIn('Parser key "val":', err.getvalue())
 
 
     def test_error_handler_property(self):