b428da70fca42bfcb091657f4aa29f7dd72c274d
Mauricio Villegas <mauricio_ville@yahoo.com>
2022-07-11 06:56:44 +0200

    Fix issues in github actions.

--- jsonargparse_tests/base.py
+++ jsonargparse_tests/base.py
@@ -16,7 +16,7 @@ if responses_available:
 else:
     def nothing_decorator(func):
         return func
-    responses_activate = nothing_decorator
+    responses_activate = nothing_decorator  # type: ignore
 
 
 is_posix = os.name == 'posix'
