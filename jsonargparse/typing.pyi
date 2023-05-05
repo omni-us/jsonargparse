e23d7bac05fc927d5dc3f9aebafd50dd28bf3e5c
Jirka <jirka.borovec@seznam.cz>
2023-05-05 23:20:42 +0200

    noqa

--- jsonargparse/typing.pyi
+++ jsonargparse/typing.pyi
@@ -2,6 +2,6 @@ import sys
 from importlib.util import find_spec
 
 if sys.version_info[:2] >= (3, 8):
-    pass
+    from typing import final  # noqa
 elif find_spec('typing_extensions'):
-    pass
+    from typing_extensions import final  # noqa
