85efc4545d2c8acf10a1edeb132ae779cf69ab2e
Jirka <jirka.borovec@seznam.cz>
2023-06-15 00:31:19 +0200

    stop subclass dive if you hit bad import

--- jsonargparse/typehints.py
+++ jsonargparse/typehints.py
@@ -918,7 +918,10 @@ def get_all_subclass_paths(cls: Type) -> List[str]:
         return "._" in class_path
 
     def add_subclasses(cl):
-        class_path = get_import_path(cl)
+        try:
+            class_path = get_import_path(cl)
+        except (ImportError, AttributeError):  # Attribute is added in case of dot notation imports
+            return
         if is_local(cl) or issubclass(cl, LazyInitBaseClass):
             return
         if not (inspect.isabstract(cl) or is_private(class_path)):
