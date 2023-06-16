619805ef46be415f49c0e8ddcbfa153eab9897f0
Jirka <jirka.borovec@seznam.cz>
2023-06-16 12:33:15 +0200

    rev

--- jsonargparse/typehints.py
+++ jsonargparse/typehints.py
@@ -4,7 +4,6 @@ import inspect
 import os
 import re
 import sys
-import traceback
 from argparse import ArgumentError
 from collections import abc, defaultdict
 from contextlib import contextmanager, suppress
@@ -921,8 +920,8 @@ def get_all_subclass_paths(cls: Type) -> List[str]:
     def add_subclasses(cl):
         try:
             class_path = get_import_path(cl)
-        except (ImportError, AttributeError):  # Attribute is added in case of dot notation imports
-            warning(f"Hit failing import with following trace: {traceback.format_exc()}")
+        except (ImportError, AttributeError) as err:  # Attribute is added in case of dot notation imports
+            warning(f"Hit failing import with following trace: {err}")
             return
         if is_local(cl) or issubclass(cl, LazyInitBaseClass):
             return
